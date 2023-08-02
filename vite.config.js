7652fc5af3e80b02806350f2ca75d38e9fc058d3
陳益昇 <fox29582@yahoo.com.tw>
2023-08-02 23:35:19 +0800

    first deploy

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/web-layout-training-vite/',
+  base: '/week4_glasses_website_vite/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
